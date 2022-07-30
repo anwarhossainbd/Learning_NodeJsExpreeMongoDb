21b690c87c05e04da715961a295f808e00aa2128
Anwar Hossain <mdanwarhossain1996bd@gmail.com>
2022-07-30 09:08:42 +0600

    Basic Import export

---
+++ index.js
@@ -0,0 +1,7 @@
+const Alldata = require("./student")
+
+console.log("Anwarr")
+
+console.log(Alldata.getAge()) ;
+
+
---
+++ student.js
@@ -0,0 +1,14 @@
+exports.getName =()=>{
+    return "Anwar Hossain" ;
+}
+
+const getAge =()=>{
+    return 27 ;
+}
+
+const cgpa =3.06;
+
+module.exports = {
+    getAge,
+    cgpa
+}
