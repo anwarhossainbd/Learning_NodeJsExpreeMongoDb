3b20d325e93c5819fe14ee0316599e6f2b5cd27f
Anwar Hossain <mdanwarhossain1996bd@gmail.com>
2022-07-30 22:23:26 +0600

    Read File

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const fs =require('fs');
 
-fs.appendFile('anwar.txt',"Go ahead ",(err)=>{
+fs.readFile('anwar.txt',"utf-8",(err,data)=>{
    if (err){
-    console.log(error)
+    console.log(err)
    }
 
    else{
-    console.log("SuccessFull")
+    console.log(data)
    }
 })
