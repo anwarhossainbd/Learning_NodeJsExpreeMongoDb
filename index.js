11c60cd79956d79eb7f427aed89f2dcf258fcf99
Anwar Hossain <mdanwarhossain1996bd@gmail.com>
2022-08-03 23:10:37 +0600

    Basic routing node and express

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-const getRandomFruitsName = require("random-fruits-name")
-var movinames = require("random-fruits-name")
 
-console.log(getRandomFruitsName()) ;
+const express = require('express');
+const app =express();
+
+app.get('/',(req,res)=>{
+    res.send("This is home page")
+});
+
+app.post('/',(req,res)=>{
+    console.log(req.body) ;
+    res.send("This is home page  with post requests ")
+});
+
+app.listen(3000,()=>{
+    console.log("listening on port 3000")
+})
+
